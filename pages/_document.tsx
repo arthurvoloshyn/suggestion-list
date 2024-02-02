503196db2c4bedbfd327f32fb46fb544ab318401
arturvoloshyn <arturvoloshyn@gmail.com>
2024-02-02 18:54:41 +0200

    refactor(pages): delete comments about Document comp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,28 +36,6 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
   const originalRenderPage = ctx.renderPage;
 
   // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
